66d0126b9289ffbf28d72204395b83c0bf597d97
Mark Rowel Tanio <work.marktanio@gmail.com>
2024-06-17 06:12:06 +0800

    updated files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-  
-    ScrollSmoother.create({
-      wrapper: '.wrapper',
-      content: '.content',
-      smooth: 1,
-      effects: true,
-      horizontal: true,
-    });
-  
-    // For horizontal scroll, we need to set the height of the wrapper to be the width of the content
-    document.querySelector('.wrapper').style.height = document.querySelector('.content').scrollWidth + 'px';
-  });
-  
+
+
+const container = document.querySelector(".container");
+const sections = gsap.utils.toArray(".container section");
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 0.5,
+    end: "+=3000",
+    snap: {
+        snapTo: 1 / (sections.length - 1), // Calculate snapping points
+        duration: { min: 0.2, max: 0.8 }, // Specify snap duration range
+        ease: "power4" // Use easing for smoother snap
+      }
+  }
+});
+
+
+
+
